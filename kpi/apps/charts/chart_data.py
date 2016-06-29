6631277cf9e8355b70bda23b315a989b130b54b9
Alex Dorey <dorey415@gmail.com>
2016-06-29 12:38:57 -0400

    * proper import * only use cached available kuids once

--- kpi/apps/charts/chart_data.py
+++ kpi/apps/charts/chart_data.py
@@ -1,13 +1,13 @@
-from charts.constants import (CHARTABLE_TYPES,
-                              SPECIFIC_CHARTS_KEY, DEFAULT_CHARTS_KEY
-                              )
+from .constants import (CHARTABLE_TYPES,
+                        SPECIFIC_CHARTS_KEY, DEFAULT_CHARTS_KEY
+                        )
 
 from pymongo import MongoClient
 kobo_instances = MongoClient().formhub.instances
 
 
-def _kuids(asset):
-    if not hasattr(asset, '_available_chart_uids'):
+def _kuids(asset, cache=False):
+    if not cache or not hasattr(asset, '_available_chart_uids'):
         survey = asset.content.get('survey', [])
         asset._available_chart_uids = [
             row.get('$kuid') for row in survey
@@ -30,7 +30,7 @@ def _data(asset, kuids):
     #                                     run_calculations=necessary_calculations,
     #                                     ).get_stats_as_object()
 
-    available_kuids = set(_kuids(asset)) & set(kuids)
+    available_kuids = set(_kuids(asset, cache=True)) & set(kuids)
     default_style = asset.chart_styles[DEFAULT_CHARTS_KEY]
     specified = asset.chart_styles[SPECIFIC_CHARTS_KEY]
     chart_styles = dict([(kuid, specified[kuid])
