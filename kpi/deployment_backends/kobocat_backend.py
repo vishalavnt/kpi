bcf823146f4773990b927c5ea1a46411cf1385ee
Alex Dorey <dorey415@gmail.com>
2016-10-17 15:15:12 -0400

    typo x2

--- kpi/deployment_backends/kobocat_backend.py
+++ kpi/deployment_backends/kobocat_backend.py
@@ -248,7 +248,7 @@ class KobocatDeploymentBackend(BaseDeploymentBackend):
                 raise Exception('The identifier is not properly formatted.')
 
         url = self.external_to_internal_url(u'{}/api/v1/forms'.format(server))
-        csv_io = self.to_xls_io(self.asset.to_xls_io(versioned=True), id_string)
+        csv_io = self.to_csv_io(self.asset.to_xls_io(versioned=True), id_string)
         valid_xlsform_csv_repr = csv_io.getvalue()
         payload = {
             u'text_xls_form': valid_xlsform_csv_repr,
