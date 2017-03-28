46021b32729ceef95e9c5ea146f2d37b8bbd2976
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2017-03-28 13:41:37 -0500

    Mock out some imported packages when building docs.

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,25 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import django
-django.setup()
+
+# Mock out some packages imported in the code.
+# See http://read-the-docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['django']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ------------------------------------------------
 
