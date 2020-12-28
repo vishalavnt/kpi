a5e61e89b9ee2458437e654fdf73a804a1436410
Agus Hilman <gushil@gmail.com>
2020-12-28 16:48:30 +0700

    OC-13977 Fixed check-nginx script

--- scripts/check-nginx/check-nginx.sh
+++ scripts/check-nginx/check-nginx.sh
@@ -12,8 +12,8 @@ echo "Not spv nginx count = $NOT_SPV_NGINX_COUNT"
 if [ $NOT_SPV_NGINX_COUNT -gt 0 ]; then
     echo "Ready to stop spv nginx processes on $(date)"
     docker exec kobo-docker_nginx_1 sv stop nginx
-    echo STOP_SUCCESS_MSG
-    echo STOP_SUCCESS_MSG >> /var/log/nginx/check-nginx.success.log 2>&1
+    echo $STOP_SUCCESS_MSG
+    echo $STOP_SUCCESS_MSG >> /var/log/nginx/check-nginx.success.log 2>&1
 else
     echo "Keep spv nginx processes alive on $(date)"
 fi
